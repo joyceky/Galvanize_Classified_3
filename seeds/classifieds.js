d0275388418200f8dc9f64b43a364a04673ed287
Kyle Joyce <kyyjoyce@gmail.com>
2017-02-15 16:44:51 -0700

    styling complete

--- seeds/classifieds.js
+++ seeds/classifieds.js
@@ -8,23 +8,59 @@ exports.seed = function(knex, Promise) {
         // Inserts seed entries
         knex('classifieds').insert({
           id:1,
-          title:'NES Classic',
-          description:'I got lucky and found it, and decided to charge 10x what it was worth.',
-          price:600,
-          item_image:'http://www.nintendo.com/images/page/nes-classic/nes-classic-edition-box.png',
+          title:'Bougainvillea',
+          description:'This flowering bush is enormous. Please come dig it up.',
+          price:200,
+          item_image:'http://cf.ltkcdn.net/garden/images/std/187241-425x319-bougainvillea.jpg',
           created_at: new Date('2016-06-26T14:26:16.000Z'),
           updated_at: new Date('2016-06-26T14:26:16.000Z')
       }),
         knex('classifieds').insert({
           id:2,
-          title:'Pikachu 9" Plush Stuffed Toy',
-          description:'Polyester fiber construction Officially licensed.',
+          title:'Basil',
+          description:'Lots of baby basil plants for sale!',
           price:10,
-          item_image:'https://images-na.ssl-images-amazon.com/images/I/41VwGotRZsL._SY300_.jpg',
+          item_image:'http://breeding.rutgers.edu/wp-content/gallery/basil/5873400949_170b16b439_o.jpg',
           created_at: new Date('2016-06-26T14:26:16.000Z'),
           updated_at: new Date('2016-06-26T14:26:16.000Z')
-      })
-      ])
+      }),
+        knex('classifieds').insert({
+          id:3,
+          title:'Large Tree',
+          description:'You will probably need a bulldozer to bring home this tree.',
+          price:1000,
+          item_image:'https://www.craftsy.com/blog/wp-content/uploads/2013/04/Screen-Shot-2013-04-30-at-12.59.19-PM.png',
+          created_at: new Date('2016-06-26T14:26:16.000Z'),
+          updated_at: new Date('2016-06-26T14:26:16.000Z')
+      }),
+      knex('classifieds').insert({
+        id:4,
+        title:'Cactus',
+        description:'Can anyone help identify this mystery cactus?',
+        price:0,
+        item_image:'https://s-media-cache-ak0.pinimg.com/736x/b6/2a/f0/b62af008cc05867069419faa552dbd15.jpg',
+        created_at: new Date('2016-04-26T14:22:16.000Z'),
+        updated_at: new Date('2016-04-26T14:22:16.000Z')
+    }),
+      knex('classifieds').insert({
+        id:5,
+        title:'Orchids',
+        description:'Selling orchids for spring!',
+        price:25,
+        item_image:'http://westerlayorchids.dreamhosters.com/wp-content/uploads/2013/03/3-Orchids-SS.jpg',
+        created_at: new Date('2016-02-26T14:26:16.000Z'),
+        updated_at: new Date('2016-02-26T14:26:16.000Z')
+    }),
+      knex('classifieds').insert({
+        id:6,
+        title:'Weird Plant',
+        description:'Can you identify this odd plant?',
+        price:25,
+        item_image:'https://s-media-cache-ak0.pinimg.com/originals/be/76/83/be76833b821261ccdd51eae1519255af.jpg',
+        created_at: new Date('2016-01-26T14:26:16.000Z'),
+        updated_at: new Date('2016-01-26T14:26:16.000Z')
+    })
+    ])
       .then(function(){
       return knex.raw(
         "SELECT setval('classifieds_id_seq', (SELECT MAX(id) FROM classifieds))"
